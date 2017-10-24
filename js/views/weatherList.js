bd2c029d1d128837d44af7fac0e27ec418f8cb6c
ariffw <mpopova@Marinas-Mac-mini.local>
2017-10-24 12:04:22 +0300

    Integration with handlebar

--- js/views/weatherList.js
+++ js/views/weatherList.js
@@ -6,20 +6,8 @@ $(document).ready( function(){
             // this.el.html("Type to search for city weather");
         },
         renderList: function( collection ){
-            // This function is called when the collection "listens"
-            // for the "refresh" event which is called in our loadResults()
-            // Now we want to compile our underscore template
-            // The underscore template just takes a string of text/html 
-            var compiled_template = _.template( $("#weatherPerDay").html() );
-            // Once compiled we can call our template and pass it any 
-            // matching data we have and append it to our view.el
-            collection.weatherList.el.html( compiled_template( { weathers: collection.models } ) );
-
-            // Compilation with Handlebar, but unfortunately return error - .toJSON() is not defined  ;(
-            //  var tpl = function () {
-            //      return Handlebars.compile($("#weatherPerDay").html());
-            // }
-            // collection.weatherList.el.html(tpl(collection.models.model.toJSON()));
+            var template = Handlebars.compile( $("#weatherPerDay").html() );
+            collection.weatherList.el.html( template({ weathers: collection.models }) );
         }
     });
 })
